ada3c630a86bd7e2c64c47937d2d314580e4b2b9
Akami14 <98305306+Akami14@users.noreply.github.com>
2023-02-24 13:09:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self):
+        self._score = 0
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, s):
+        if 0 <= s <= 100:
+            self._score = s
+        else:
+            raise ValueError('The score must be between 0 ~ 100!')
+
+
+a = Student()
+a.score = 9
+print(a.score)
